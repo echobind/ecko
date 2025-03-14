22a5e8acbea4829ac533e9e9a9913394b220456e
Cully Larson <cully.larson@gmail.com>
2025-03-14 12:08:57 -0700

    Add an example test suite using Stripe.

--- readme.md
+++ readme.md
@@ -10,10 +10,8 @@ npm install ecko
 
 ## TODO
 
-- [ ] Add github workflow to run checks and tests
 - [ ] Add docs
 - [ ] Add examples
-- [ ] Add more tests
 - [ ] Consider supporting an "all" request method. This could make response
       frequency unpredictable. Also, you should know the request method for the
       endpoint; why would you need "all?
---
+++ tests/stripe-example.test.ts
@@ -0,0 +1,85 @@
+import { test, expect, afterAll, beforeAll, afterEach } from "vitest";
+import { Stripe } from "stripe";
+import { EckoServer } from "../src/index.js";
+import { type EckoApi } from "../src/api.js";
+
+const PORT = 3005;
+
+let ecko: EckoApi;
+let eckoServer: EckoServer;
+let stripe: Stripe;
+
+beforeAll(() => {
+  eckoServer = EckoServer();
+
+  const startResult = eckoServer.start({ port: PORT, logLevel: "error" });
+
+  ecko = startResult.ecko;
+
+  const url = new URL(startResult.baseUrl);
+
+  stripe = new Stripe("does_not_matter", {
+    apiVersion: "2025-02-24.acacia",
+    host: url.hostname,
+    port: url.port,
+    protocol: "http",
+  });
+});
+
+afterEach(() => {
+  eckoServer.reset();
+});
+
+afterAll(() => {
+  eckoServer.stop();
+});
+
+test("Should fetch a balance from the mocked Stripe API", async () => {
+  ecko.register("/v1/balance", "get", {
+    frequency: "always",
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    payload: JSON.stringify({
+      object: "balance",
+      available: [
+        {
+          amount: 666670,
+          currency: "usd",
+          source_types: {
+            card: 666670,
+          },
+        },
+      ],
+      connect_reserved: [
+        {
+          amount: 0,
+          currency: "usd",
+        },
+      ],
+      livemode: false,
+      pending: [
+        {
+          amount: 61414,
+          currency: "usd",
+          source_types: {
+            card: 61414,
+          },
+        },
+      ],
+    }),
+  });
+
+  const balance = await stripe.balance.retrieve();
+
+  expect(balance).toBeDefined();
+  expect(balance.object).toBe("balance");
+  expect(balance.available).toBeDefined();
+  expect(balance.available[0].amount).toBe(666670);
+  expect(balance.available[0].currency).toBe("usd");
+  expect(balance?.available?.[0]?.source_types?.card).toBe(666670);
+  expect(balance.connect_reserved).toBeDefined();
+  expect(balance?.connect_reserved?.[0]?.amount).toBe(0);
+  expect(balance?.connect_reserved?.[0]?.currency).toBe("usd");
+});
